6eedcaaac6ce581e77c3a403a6da9166a86b879f
as-devy <elbedwehykhaled@gmail.com>
2025-02-14 20:37:06 +0200

    updates

--- main.js
+++ main.js
@@ -1,35 +1,13 @@
-const img_model = document.querySelector(".img_model")
-const model_img = document.querySelector(".img_model img")
+const mobileMenu = document.querySelector("#mobile_menu");
+const burger_menu = document.querySelector("#burger_menu");
 
-document.querySelectorAll(".personal .certificates img").forEach(img=>{
-    img.addEventListener("click", ()=>{
-       img_model.querySelector("img").src = img.src;
-       img_model.classList.add("active")
-    })
+burger_menu.addEventListener("click", ()=>{
+    console.log("burger_menu")
+    mobileMenu.classList.toggle("active")
 })
 
-img_model.querySelector("#close").addEventListener("click", ()=>{
-    img_model.classList.remove("active")
-})
-
-img_model.addEventListener("click", (e)=> {
-    if (event.target !== img_model.querySelector("img")) {
-        img_model.classList.remove("active");
+document.addEventListener("click", function (event) {
+    if (!mobileMenu.contains(event.target) && !burger_menu.contains(event.target)) {
+        mobileMenu.classList.remove("active");
     }
 });
-
-// let scale = 1;
-// const scaleStep = 0.2;
-// const maxScale = 1.5;
-// const minScale = 1;
-
-// model_img.addEventListener("click", function () {
-//     if (scale === 1) {
-//         scale = maxScale;
-//         model_img.style.cursor = "zoom-out";
-//     } else {
-//         scale = minScale;
-//         model_img.style.cursor = "zoom-in";
-//     }
-//     model_img.style.transform = `scale(${scale})`;
-// });
