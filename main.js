27279f3b51f81493f3dcfddba2acc62eb73d5512
as-devy <elbedwehykhaled@gmail.com>
2025-03-15 21:50:36 +0200

    updates

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
+window.onload = ()=>{
+    document.querySelector(".pre_loader").classList.add("disactive")
+}
+
 const mobileMenu = document.querySelector("#mobile_menu");
 const burger_menu = document.querySelector("#burger_menu");
 
-burger_menu.addEventListener("click", ()=>{
-    console.log("burger_menu")
-    mobileMenu.classList.toggle("active")
-})
+if (burger_menu){
+    burger_menu.addEventListener("click", () => {
+        console.log("burger_menu")
+        mobileMenu.classList.toggle("active")
+    })
+    
+}
+
 
 document.addEventListener("click", function (event) {
     if (!mobileMenu.contains(event.target) && !burger_menu.contains(event.target)) {
@@ -12,6 +20,8 @@ document.addEventListener("click", function (event) {
     }
 });
 
-document.querySelectorAll(".PPhIP").forEach(ele => {
-    ele.style.display = 'none';
-})
+if (document.querySelectorAll(".PPhIP")) {
+    document.querySelectorAll(".PPhIP").forEach(ele => {
+        ele.style.display = 'none';
+    })
+}
